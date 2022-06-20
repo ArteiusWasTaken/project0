b0d398c717233c5e25b17c222b026a0ee98bb513
ArteiusWasTaken <sebastiancifer@gmail.com>
2022-06-20 18:41:46 -0500

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import { BaseLayout } from "../components/layout";
+
+const Home: NextPage = () => {
+  return (
+    <BaseLayout>
+
+    </BaseLayout>
+  );
+};
+
+export default Home;
